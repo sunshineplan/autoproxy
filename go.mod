d4a07550bb7782ecd04b9a15768bef1e28ea2a3e
sunshineplan <sunshineplan@users.noreply.github.com>
2025-12-31 15:18:29 +0800

    Use bufio package instead of third-party package

--- go.mod
+++ go.mod
@@ -1,5 +1,3 @@
 module autoproxy
 
 go 1.25
-
-require github.com/sunshineplan/utils v0.1.83
--- main.go
+++ main.go
@@ -1,12 +1,11 @@
 package main
 
 import (
+	"bufio"
 	"log"
 	"net/http"
 	"os"
 	"strings"
-
-	"github.com/sunshineplan/utils/txt"
 )
 
 func main() {
@@ -15,43 +14,42 @@ func main() {
 		log.Fatal(err)
 	}
 	defer resp.Body.Close()
-
-	res, err := txt.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatal(err)
-	}
 	f, err := os.Create("autoproxy.txt")
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer f.Close()
-
-	w := txt.NewWriter(f)
-	w.WriteLine("[AutoProxy 0.2.9]")
-	w.WriteLine("! Last Modified: $time")
-	w.WriteLine("! Expires: 24h")
-	//w.WriteLine("! HomePage: https://github.com/sunshineplan/autoproxy")
-	//w.WriteLine("! GitHub URL: https://raw.githubusercontent.com/sunshineplan/autoproxy/release/autoproxy.txt")
-	//w.WriteLine("! jsdelivr URL: https://cdn.jsdelivr.net/gh/sunshineplan/autoproxy@release/autoproxy.txt")
-	w.WriteLine("")
-	for _, i := range res {
+	w := bufio.NewWriter(f)
+	w.WriteString("[AutoProxy 0.2.9]\n")
+	w.WriteString("! Last Modified: $time\n")
+	w.WriteString("! Expires: 24h\n")
+	//w.WriteString("! HomePage: https://github.com/sunshineplan/autoproxy\n")
+	//w.WriteString("! GitHub URL: https://raw.githubusercontent.com/sunshineplan/autoproxy/release/autoproxy.txt\n")
+	//w.WriteString("! jsdelivr URL: https://cdn.jsdelivr.net/gh/sunshineplan/autoproxy@release/autoproxy.txt\n")
+	w.WriteString("\n")
+	scanner := bufio.NewScanner(resp.Body)
+	for scanner.Scan() {
+		i := scanner.Text()
 		if strings.HasSuffix(i, "@cn") {
 			continue
 		}
 		i = strings.ReplaceAll(i, ":@ads", "")
 		switch {
 		case strings.HasPrefix(i, "domain:"):
-			w.WriteLine(strings.Replace(i, "domain:", "||", 1))
+			w.WriteString(strings.Replace(i, "domain:", "||", 1) + "\n")
 		case strings.HasPrefix(i, "full:"):
-			w.WriteLine(strings.Replace(i, "full:", "|http://", 1))
-			w.WriteLine(strings.Replace(i, "full:", "|https://", 1))
+			w.WriteString(strings.Replace(i, "full:", "|http://", 1) + "\n")
+			w.WriteString(strings.Replace(i, "full:", "|https://", 1) + "\n")
 		case strings.HasPrefix(i, "keyword:"):
-			w.WriteLine(strings.Replace(i, "keyword:", "", 1))
+			w.WriteString(strings.Replace(i, "keyword:", "", 1) + "\n")
 		case strings.HasPrefix(i, "regexp:"):
-			w.WriteLine(strings.Replace(i, "regexp:", "/", 1) + "/")
+			w.WriteString(strings.Replace(i, "regexp:", "/", 1) + "/" + "\n")
 		default:
 			log.Println("unknow format:", i)
 		}
+	}
+	if err := scanner.Err(); err != nil {
+		log.Fatal(err)
 	}
 	w.Flush()
 }
